ec4b5b43d16cb693281eec9a27ee1fee3258c0c6
dggiazureports-afk <dggiazureports@gmail.com>
2026-01-16 15:13:47 +0530

    Add script to sum integers from 1 to 50

--- second project.py
+++
@@ -1,9 +0,0 @@
-# Take user's first name and last name as input
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-# Concatenate first name and last name into a full name
-full_name = first_name + " " + last_name
-
-# Print a personalized greeting message
-print(f"Hello, {full_name}! Welcome!")
